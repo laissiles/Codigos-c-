1c57f28312c937139b1ce70d506ce0c6df20030f
Lais Siles <laislevine@gmail.com>
2019-03-25 10:37:11 -0300

    Add files via upload

---
+++ exercicio2/exercicio2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Elabore um programa que mostre seu nome.
+
+            Console.WriteLine("Digite seu nome: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Seu nome é: " + nome);
+
+            Console.ReadKey();
+        }
+    }
+}
