4a29ef9bdb30b5bf16d93e99f100399b115d771a
Lais Siles <laislevine@gmail.com>
2019-03-25 10:42:32 -0300

    Add files via upload

---
+++ exercicio01/exercicio01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace exercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+            //Faça um programa que leia nome, cod, valor e quantidade de uma peça e retorne seu codigo e seu valor total//
+        {
+            Console.WriteLine("Digite o nome da peça:");
+            string nomePeca = Console.ReadLine();
+            Console.WriteLine("Digite o código da peça:");
+            int codPeca = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor da peça:");
+            float valorPeca = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a quantidade de peças:");
+            int qtaPeca = int.Parse(Console.ReadLine());
+
+            float resultado = qtaPeca * valorPeca;
+            Console.WriteLine("O código da sua peça é: " +codPeca+" e o Valor total das peças é: " +resultado);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ exercicio3/exercicio3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace exercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Elabore um programa que peça ao usuário seu nº preferido e depois o exiba.//
+
+            Console.WriteLine("Digite seu numero predileto: ");
+            float numero = float.Parse(Console.ReadLine());
+            Console.WriteLine("O número digitado por voce foi: " + numero);
+            Console.ReadKey();
+        }
+    }
+}
